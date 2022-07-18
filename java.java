20971d367eedd1b6e74e7734dc6a76c6460ad35d
viishnurs <85409531+viishnurs@users.noreply.github.com>
2022-07-18 17:57:02 +0530

    Update java.java

--- java.java
+++ java.java
@@ -3,6 +3,6 @@ class Simple
     public static void main(String args[])
     {
 
-System.out.println("hello this is vishnu codingi in kali linux of java");
+System.out.println("hello this is vishnu coding in kali linux of java");
     }
 }
